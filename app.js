da292d8f8f9b27207856cc547f4fe0a8dac3eead
AldramA <mu.aldurama@gmail.com>
2024-03-27 08:45:31 -0400

    merge frontend together

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
+const path = require("path");
 const ex = require("express");
 const app = ex();
 
+app.use(ex.static(path.join(__dirname, "static")));
 
+app.get("/tasks", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 
-const port = process.env.PORT ||5500;
+const port = process.env.PORT || 5500;
 const start = async () => {
   try {
     app.listen(port, console.log(`Server is listening on port ${port}`));
